ba3eefdd558d32280179ccf21a0e9fdc52c11c3a
Kalina Nenova <kalina.nenova@gmail.com>
2018-06-07 19:45:24 +0200

    Solution of the "Checkbox"-Problem

--- HTML4Experiment/WAN/submit.php
+++ HTML4Experiment/WAN/submit.php
@@ -9,15 +9,15 @@
 		$tos = $_POST['tos'];
 		$contact = $_POST['contact'];
 		$res = $_POST['res'];
-		$meth = $_POST['meth'];
+		$meth = implode(',',$_POST['meth']);		
 		$sec = $_POST['sec'];
 		$req1 = $_POST['req1'];
 		$req2 = $_POST['req2'];
 		$req3 = $_POST['req3'];
 		$req4 = $_POST['req4'];
 		
-		echo $meth;
-		echo "/n";
+		echo gettype($meth);
+		echo "\n";
 		echo $api, " ", $desc," ", $accessAPI," ", $version," ", $tos," ", $contact," ", $res," ", $meth," ", $sec," ", $req1," ", $req2," ", $req3," ", $req4;
  
 		if(mysqli_query($conn, "INSERT INTO solutions VALUES(NULL, '$api', '$desc', '$accessAPI', '$version', 
